abfd8393c0671a0473be48b00fe63efcdfb5f197
yerin <yelin7417@gmail.com>
2026-03-30 15:21:39 +0900

    2차 수

--- array-1/2array-2.c
+++ array-1/2array-2.c
@@ -3,15 +3,13 @@
 int main(void)
 {
     int i;
-    char menu[5][10] = { //5행 10열(0~4행 0~9열)
+    char menu[3][10] = { //5행 10열(0~4행 0~9열)
         "init", //1행씩 초기값 설정
         "open",
-        "close",
-        "read",
-        "write"
+        "close"
     };
 
-    for (i = 0; i < 5; i++) //0~4
+    for (i = 0; i < 3; i++) //0~4
         printf("%d 번째 메뉴: %s \n", i, menu[i]); //menu[i]는 행을 의미
 
     return 0;

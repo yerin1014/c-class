0513390de711ea09cb3a4624f66caf0bf00673da
yerin <yelin7417@gmail.com>
2026-03-30 14:47:08 +0900

    문자 배열 실습

---
+++ array-1/char-array-1.c
@@ -0,0 +1,17 @@
+﻿// 문자열의 길이를 구하는 프로그램
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+    char str[30] = "C language"; //문자배열 30자
+    int i = 0; //0부터 시작
+
+    while (str[i] != '\0') //조건이 만족할 때 반복
+                           //!= 다르다 \0는 Null 값을 의미
+                           //문자열 내용 끝나는 시점에 반드시 Null이 들어감.
+        i++;
+
+    printf("문자열\"%s\"의 길이는 %d입니다.\n", str, i);
+    return 0;
+}

87a573140793a4d9f04c2ee3b33b6debfd89067f
yerin <yelin7417@gmail.com>
2026-03-30 14:44:32 +0900

    2차원 배열

---
+++ array-1/2array-2.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   
+#include <stdio.h>
+int main(void)
+{
+    int i;
+    char menu[5][10] = { //5행 10열(0~4행 0~9열)
+        "init", //1행씩 초기값 설정
+        "open",
+        "close",
+        "read",
+        "write"
+    };
+
+    for (i = 0; i < 5; i++) //0~4
+        printf("%d 번째 메뉴: %s \n", i, menu[i]); //menu[i]는 행을 의미
+
+    return 0;
+}

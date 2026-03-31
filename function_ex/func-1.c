5f6a5b57ff3042aa8c7b9a9f7e0ab4530c840c00
yerin <yelin7417@gmail.com>
2026-03-31 12:39:23 +0900

    1차 수정 (함수)

--- function_ex/func-1.c
+++ function_ex/func-1.c
@@ -1,36 +1,43 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int Add(int a, int b);
-int Input(void);
+int Add(int a, int b); //함수 원형
+int Input(void); //; 꼭 붙임
 void Result_Print(int val);
 void Intro(void);
 
-int main() {
+int main() { //시작
 	int a, b, result;
-	Intro();
-	a = Input();
-	b = Input();
-	result = Add(a, b);
-	Result_Print(result);
+
+	Intro(); // Intro()함수 호출, ()안에 인수가 없음.
+	//함수 앞에 type이 없음, 반환되는 값이 없음.
+	a = Input(); //Input()함수 호출, ()안에 인수가 없음.
+	b = Input(); //Input()함수 호출, ()안에 인수가 없음.
+	result = Add(a, b); //Add() 함수 호출, 인수 있음.(정수형 a, b)
+	Result_Print(result); //Result_Print  호출, 인수 하나(정수형)
 	return 0;
 }
 
-void Intro(void) {
-	printf("****** START *****\n");
+void Intro(void) { //함수, ()안에 매개변수가 없음.
+	//반환되는 type이 없음, 반환되는 값이 없음.
+	printf("****** START *****\n"); //제목 출력 부분.
 	printf("두 개의 정수 입력: \n");
 }
 
-int Input(void) {
-	int input;
-	scanf("%d", &input);
-	return input;
+int Input(void) { //사용자 정의 함수, 매개변수 없음, 정수 입력 부분.
+	int input; //변수 이름
+	scanf("%d", &input); //정수형 숫자 입력
+	return input; //값을 반환(보냄)
+	//int input() -> int는 반환되는 값의 형 의미.
 }
 
-int Add(int i, int j) {
-	return i + j;
+int Add(int i, int j) { //int i, int j -> 매개변수(type 선언)
+	// a -> i, b -> j
+	return i + j; //더한 값을 반환(int형)
+	//int Add(int i, int j) -> int
 }
 
-void Result_Print(int val) {
+void Result_Print(int val) {  //result -> 매개변수 val(정수형)
 	printf("덧셈에 대한 결과: %d \n", val);
 	printf("****** END ******");
+	//반환되는 값이 없음 -> void
 }

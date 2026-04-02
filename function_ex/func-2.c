9d139d7ce3d8de294c8780750dbe90b37d3fa498
yerin <yelin7417@gmail.com>
2026-04-02 12:49:20 +0900

    함수

---
+++ function_ex/func-2.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+int fun1(); // 함수 원형(함수 선언)
+int fun2();
+
+int main() {
+	printf("메인 시작\n"); //1
+	fun1(); //2 함수 호출
+	printf("메인 끝\n"); //10
+	return 0; //11ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+}
+
+int fun1() {
+	printf("fun1함수 시작\n"); //3
+	fun2(); //4 함수 호출
+	printf("fun1함수 끝\n"); //8
+	return 0; //9
+}
+
+int fun2() {
+	printf("fun2함수 시작\n"); //5
+	printf("fun2함수 끝\n"); //6
+	return 0; //7
+}
---
+++ function_ex/recur.c
@@ -0,0 +1,26 @@
+﻿//함수는 자기 자신을 호출할 수도 있다. 이것을 순환(rexursion)라고 부른다.
+//재귀 호출(recursive call)이란 함수 내부에서 함수가 자기 자신을 또다시 호출하는 행위
+// (스스로를 호출)
+// 함수 내에 재귀 호출을 중단하도록 조건이 변경될 명령문을 반드시 포함
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int func(int); //함수 선언 추가
+int main() {
+	int a;
+	printf("숫자를 입력하세요: ");
+	scanf("%d", &a);
+	printf("%d", func(a)); //a를 호출하여 받은 결과를 출력
+
+}
+
+int func(int a) {
+	if (a <= 1) return 1;
+	//순환 호출을 멈추는 부분
+	return a * func(a - 1);
+	//자기 자신을 순환적으로 호출하다가 결과를 반환
+}
+//a = 5 5*func(4) = 5*24 = 120
+//a = 4 4*func(3) = 4*6 = 24
+//a = 3 3*func(2) = 3*2 = 6
+//a = 2 2*func(1) = 2*1 = 2
